042f8e0d8d84e9eced4614ec5b21caadc21730a5
opoto <olivier.potonniee@gmail.com>
2021-01-18 22:12:11 +0100

    Add step counter check box + sensor list

--- app/src/main/java/org/opoto/testdroid/MainActivity.java
+++ app/src/main/java/org/opoto/testdroid/MainActivity.java
@@ -1,5 +1,8 @@
 package org.opoto.testdroid;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.security.keystore.KeyGenParameterSpec;
@@ -17,9 +20,12 @@ import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
+import java.security.Principal;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 import javax.security.auth.x500.X500Principal;
 
@@ -52,15 +58,55 @@ public class MainActivity extends AppCompatActivity {
         TextView deviceInfo = findViewById(R.id.device_info);
         deviceInfo.setText(s);
 
+        // VARIOUS
+
+        TextView miscTxt = findViewById(R.id.misc_txt);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        String SensorTxt = "";
+        for (Sensor _sensor: sensors) {
+            SensorTxt += _sensor.getName() + "\n";
+        }
+        final CheckBox stepCounter = findViewById(R.id.step_counter);
+        stepCounter.setChecked(sensor != null);
+        miscTxt.setText(SensorTxt);
+
         // ============================== KEYSTORE TESTS
+
 
         final CheckBox androidKeystore = findViewById(R.id.android_keystore);
         final CheckBox hardwareBacked = findViewById(R.id.hardware_backed);
         final CheckBox keyAttestation = findViewById(R.id.key_attestation);
         final CheckBox strongbox = findViewById(R.id.strongbox);
 
-        try {
+        Date now = new Date();
+        Date originationEnd = new Date(now.getTime() + 0); // from now
+        Date consumptionEnd = new Date(now.getTime() + (1000 * 24 * 365 * 2)); // 2 years
 
+        // Strongbox
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance(
+                    KeyProperties.KEY_ALGORITHM_EC, ANDROID_KEYSTORE);
+            KeyGenParameterSpec.Builder kps = new KeyGenParameterSpec.Builder(
+                    MY_KEY,
+                    KeyProperties.PURPOSE_SIGN | KeyProperties.PURPOSE_VERIFY)
+                    .setDigests(KeyProperties.DIGEST_NONE, KeyProperties.DIGEST_SHA256)
+                    .setKeyValidityStart(now)
+                    .setKeyValidityForOriginationEnd(originationEnd)
+                    .setKeyValidityForConsumptionEnd(consumptionEnd);
+
+            kps.setIsStrongBoxBacked(true);
+            kpg.initialize(kps.build());
+            KeyPair keypair = kpg.generateKeyPair();
+            strongbox.setChecked(true);
+
+        } catch (Exception ex) {
+            Log.e(LOG_TAG, "StrongBox failed: " + ex.toString());
+        }
+
+        try {
             KeyStore ks = KeyStore.getInstance(ANDROID_KEYSTORE);
             ks.load(null);
             androidKeystore.setChecked(true);
@@ -72,15 +118,12 @@ public class MainActivity extends AppCompatActivity {
                     KeyProperties.PURPOSE_SIGN)
                     .setKeySize(2048)
                     .setCertificateSubject(new X500Principal("CN=unused"))
-                    .setDigests(KeyProperties.DIGEST_SHA256)
-                    .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1);
-
-            try {
-                kSpec.setIsStrongBoxBacked(true);
-                strongbox.setChecked(true);
-            } catch (Throwable err) {
-                //
-            }
+                    .setDigests(KeyProperties.DIGEST_NONE, KeyProperties.DIGEST_SHA256)
+                    .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
+                    .setKeyValidityStart(now)
+                    .setKeyValidityForOriginationEnd(originationEnd)
+                    .setKeyValidityForConsumptionEnd(consumptionEnd)
+                    .setAttestationChallenge("key attestation test".getBytes());
 
             kpg.initialize(kSpec.build());
             KeyPair keypair = kpg.generateKeyPair();
@@ -92,15 +135,15 @@ public class MainActivity extends AppCompatActivity {
             Certificate[] certificates = ks.getCertificateChain(MY_KEY);
             Certificate rootCA = certificates[certificates.length - 1];
             byte[] rootSN = ((X509Certificate) rootCA).getSerialNumber().toByteArray();
+            Principal rootSub = ((X509Certificate) rootCA).getSubjectDN();
             if (Arrays.equals(rootSN, GOOGLE_ROOT_CA_SN)) {
                 keyAttestation.setChecked(true);
             }
             Log.i(LOG_TAG, "Certs: " + certificates.length );
-            Log.i(this.getClass().getSimpleName(), "root=" + Arrays.toString(rootSN));
-            Log.i(this.getClass().getSimpleName(), "All checked passed");
+            Log.i(LOG_TAG, "root: SN=" + Arrays.toString(rootSN) + ", subject=" + rootSub.getName());
+            Log.i(LOG_TAG, "All checked passed");
 
             // BIOMETRICS
-
 
             final CheckBox bioAvailable = findViewById(R.id.bio_available);
             final CheckBox bioEnrolled = findViewById(R.id.bio_enrolled);
@@ -126,6 +169,8 @@ public class MainActivity extends AppCompatActivity {
 
         } catch (Exception e) {
             e.printStackTrace();
+            Log.e(LOG_TAG, e.toString());
+            miscTxt.setText("ERROR: " + e.toString());
         }
 
     }
